ebbd65c6cc198a8bb7bd9bd3b63ea571312ee30b
Gunit12345-crypto <gunit8526.beaift24@chitkara.edu.in>
2026-05-17 16:53:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
-from model import recommend
+import streamlit as st
+import pickle
 
-print("🎬 Movie Recommendation System")
+# PAGE CONFIG
+st.set_page_config(
+    page_title="Movie Recommendation System",
+    page_icon="🎬",
+    layout="centered"
+)
 
-movie_name = input("Enter movie name: ")
+# LOAD FILES
+movies = pickle.load(open('movies.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+vectors = pickle.load(open('vectors.pkl', 'rb'))
 
-try:
-    results = recommend(movie_name)
+# RECOMMEND FUNCTION
+def recommend(movie):
 
-    print("\nRecommended Movies:")
-    for movie in results:
-        print("👉", movie)
+    movie_index = movies[movies['title'] == movie].index[0]
 
-except:
-    print("❌ Movie not found, try another one")
+    distances, indices = model.kneighbors(
+        [vectors[movie_index]],
+        n_neighbors=6
+    )
+
+    recommended_movies = []
+
+    for i in indices[0][1:]:
+        recommended_movies.append(movies.iloc[i].title)
+
+    return recommended_movies
+
+# UI
+st.title("🎬 Movie Recommendation System")
+
+st.write("AI/ML based movie recommendation using NLP and KNN")
+
+selected_movie = st.selectbox(
+    "Select a movie",
+    movies['title'].values
+)
+
+if st.button("Recommend Movies"):
+
+    recommendations = recommend(selected_movie)
+
+    st.subheader("Recommended Movies")
+
+    for movie in recommendations:
+        st.write("👉", movie)
