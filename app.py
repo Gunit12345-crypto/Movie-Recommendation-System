51e55cb2b0265ee7b64305f0b28c4505e3b31570
Gunit12345-crypto <gunit8526.beaift24@chitkara.edu.in>
2026-05-22 20:48:06 +0530

    Add imports for Streamlit and data processing

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # PAGE CONFIG
 st.set_page_config(
